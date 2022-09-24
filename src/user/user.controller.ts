d15eb4ca512ab7a381741f8a490b23899c231382
Morshedpewpew <42214700+Morshedpewpew@users.noreply.github.com>
2022-09-25 05:26:28 +0600

    25 sep

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,30 +1,32 @@
 import { Delete, Get, Param, Patch, Post, Req } from "@nestjs/common";
 import { Request } from "express";
 import { Controller } from '@nestjs/common';
+import { UserService } from "./user.service";
 
 @Controller('user')
 export class UserController {
+    constructor(private userService: UserService){}
     @Get()
     getUsers() {
-      return { name: 'Morshed', email: 'i.morshed2016@gmail.com' };
+      return this.userService.get();
     }
     @Post()
     store(@Req() req:Request){
       //console.log(req.body);
-      return req.body;
+      return this.userService.create(req);
     }
     @Patch(':ID')
-    update(@Req() req:Request){
+    update(@Req() req:Request,@Param() param:{ID: number} ){
       //console.log(req.body);
-      return req.body;
+      return this.userService.update(req,param);
     }
      @Get(':ID')
-     getUser(@Param() params:{Idhastobe: number}){
-       return params;
+     getUser(@Param() param:{ID: number}){
+       return this.userService.show(param);
      }
     @Delete(':ID')
-    deleteUser(@Param() params:{Idhastobe: number}){
-      return params;
+    deleteUser(@Param() param:{ID: number}){
+        return this.userService.delete(param);
     }
 
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { Request } from 'express';
 
 @Injectable()
 export class UserService {
+    get() {
+        return { name: 'Morshed', email: 'i.morshed2016@gmail.com' };
+      }
+      create(req: Request){
+        return req.body;
+      }
+      update(req: Request,param :{ID: number}){
+        return {body:req.body,param};
+      }
+      show(param:{ID: number}){
+        return param;
+      }
+      delete(param:{ID: number}){
+        return param;
+      }
 
-    
 }
